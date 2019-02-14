57686c54d5ce3a6eadef91fdd5574ffc294a79b4
mjj94 <m.jammal94@gmail.com>
2019-02-14 15:02:13 +0100

    commencer à developper les variantes

--- VideoGenTransformer/src/VideoGenTest1XtendVersion.xtend
+++ VideoGenTransformer/src/VideoGenTest1XtendVersion.xtend
@@ -10,12 +10,17 @@ import fr.istic.videoGen.VideoText
 import java.io.File
 import fr.istic.videoGen.AlternativesMedia
 import fr.istic.videoGen.MediaDescription
+import java.util.ArrayList
+import java.util.Stack
+import java.util.HashSet
 
 class VideoGenTest1XtendVersion {
 
 	@Test
 	def void testLoadModel() {
-
+		val listMan = new ArrayList<MandatoryMedia>();
+		val listOp = new ArrayList<OptionalMedia>();
+		val listAlt = new ArrayList<AlternativesMedia>();
 		val videoGen = new VideoGenHelper().loadVideoGenerator(URI.createURI("specification.videogen"))
 		assertNotNull(videoGen)
 		for (Media m : videoGen.medias) {
@@ -25,7 +30,7 @@ class VideoGenTest1XtendVersion {
 					val des = man.description as VideoDescription
 					val f = new File(des.location)
 					println(des.location + " " + f.length)
-
+					listMan.add(man);
 				}
 			} else if (m instanceof OptionalMedia) {
 				val op = m as OptionalMedia
@@ -33,7 +38,7 @@ class VideoGenTest1XtendVersion {
 					val des = op.description as VideoDescription
 					val f = new File(des.location)
 					println(des.location + " " + f.length)
-
+					listOp.add(op);
 				}
 			} else if (m instanceof AlternativesMedia) {
 				val alt = m as AlternativesMedia
@@ -42,14 +47,155 @@ class VideoGenTest1XtendVersion {
 						val des = malt as VideoDescription
 						val f = new File(des.location)
 						println(des.location + " " + f.length)
+						listAlt.add(alt)
 					}
 				}
 			}
-//			val omedia = (m as OptionalMedia)
 		}
-//		println(omedia.description.location)
-	// and then visit the model
-	// eg access video sequences: videoGen.videoseqs
+		val result = calculateVariants(listMan, listOp, listAlt)
+//		println(result.size())
 	}
 
+	def ArrayList calculateVariants(ArrayList<MandatoryMedia> listMan, ArrayList<OptionalMedia> listOp,
+		ArrayList<AlternativesMedia> listAlt) {
+		println("listOp.size() = " + listOp.size() + " listAlt.size() = " + listAlt.size())
+
+		val resultNumber = (listOp.size() * 2) * listAlt.size();
+		var result = initResult(listMan, resultNumber)
+		var optionelCombinations = new ArrayList<ArrayList<OptionalMedia>>();
+		recursive_combinations_start(listOp, optionelCombinations);
+		var j = 0;
+		var i = 0;
+		println("size = " + result.size())
+
+		while (j < resultNumber) {
+			for (AlternativesMedia alt : listAlt) {
+				result.get(j).add(alt);
+				if (i < optionelCombinations.size()) {
+					println(optionelCombinations.get(i).size())
+					result.get(j).addAll(optionelCombinations.get(i))
+				}
+//				println(result.get(j).size())
+				j++;
+//				println("j " + j)
+//				println(i)
+			}
+			i++;
+		}
+
+		return result;
+	}
+
+	def ArrayList<ArrayList<Media>> insertOp(ArrayList<OptionalMedia> listOp, int size,
+		ArrayList<ArrayList<Media>> resultWithMan) {
+		var result = new ArrayList(resultWithMan);
+		var listOpTemp = new Stack<OptionalMedia>;
+		listOpTemp.addAll(listOp)
+		var i = 0;
+		var j = 0;
+		var c = 0;
+		var OptionalMedia head
+		var ArrayList<ArrayList<OptionalMedia>> used = new ArrayList;
+		var tail = new Stack<OptionalMedia>
+		var combo = new ArrayList<OptionalMedia>
+		while (j < resultWithMan.size()) {
+
+			if (!listOpTemp.isEmpty()) {
+				head = listOpTemp.pop()
+				tail.addAll(listOpTemp)
+			}
+			combo.add(head)
+			if (i == 0) {
+				combo.addAll(tail)
+			}
+			if (!contain(used, combo)) {
+				while (c < size) {
+					result.get(j).addAll(combo)
+					c++
+					j++
+				}
+			}
+		}
+		return result;
+	}
+
+	def boolean contain(ArrayList<ArrayList<OptionalMedia>> used, ArrayList<OptionalMedia> combo) {
+		var boolean compar = false;
+		for (ArrayList<OptionalMedia> l : used) {
+			println("l: " + l + " combo: " + combo)
+			compar = ((new HashSet<OptionalMedia>(l)).equals((new HashSet<OptionalMedia>(combo))));
+			if (compar) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	def ArrayList<ArrayList<Media>> initResult(ArrayList<MandatoryMedia> listMan, int size) {
+
+		val result = new ArrayList<ArrayList<Media>>();
+		var i = 0;
+		while (i < size) {
+			result.add(new ArrayList<Media>(listMan))
+			i++;
+		}
+
+		return result;
+	}
+
+	def void recursive_combinations(ArrayList<OptionalMedia> combination, int ndx, ArrayList<OptionalMedia> elems,
+		ArrayList<ArrayList<OptionalMedia>> result) {
+		if (ndx == elems.length) { // (reached end of list after selecting/not selecting) 
+			result.add(combination)
+		} else { // (include element at ndx) 
+			combination.add(elems.get(ndx));
+			recursive_combinations(combination, ndx + 1, elems, result); // (don't include element at ndx) 
+			combination.remove(elems.get(ndx));
+			recursive_combinations(combination, ndx + 1, elems, result);
+		}
+	}
+
+	def void recursive_combinations_start(ArrayList<OptionalMedia> elems, ArrayList<ArrayList<OptionalMedia>> result) {
+		val ArrayList<OptionalMedia> combination = new ArrayList<OptionalMedia>();
+		recursive_combinations(combination, 0, elems, result);
+		for (ArrayList<OptionalMedia> l : result) {
+			println("combSize: " + l.size())
+
+		}
+	}
+
+//	@Test
+//	def void testLoadModelOld() {
+//
+//		val videoGen = new VideoGenHelper().loadVideoGenerator(URI.createURI("specification.videogen"))
+//		assertNotNull(videoGen)
+//		for (Media m : videoGen.medias) {
+//			if (m instanceof MandatoryMedia) {
+//				val man = m as MandatoryMedia
+//				if (man.description instanceof VideoDescription) {
+//					val des = man.description as VideoDescription
+//					val f = new File(des.location)
+//					println(des.location + " " + f.length)
+//
+//				}
+//			} else if (m instanceof OptionalMedia) {
+//				val op = m as OptionalMedia
+//				if (op.description instanceof VideoDescription) {
+//					val des = op.description as VideoDescription
+//					val f = new File(des.location)
+//					println(des.location + " " + f.length)
+//
+//				}
+//			} else if (m instanceof AlternativesMedia) {
+//				val alt = m as AlternativesMedia
+//				for (MediaDescription malt : alt.medias) {
+//					if (malt instanceof VideoDescription) {
+//						val des = malt as VideoDescription
+//						val f = new File(des.location)
+//						println(des.location + " " + f.length)
+//					}
+//				}
+//			}
+//		}
+//	}
 }
--- VideoGenTransformer/xtend-gen/VideoGenTest1XtendVersion.java
+++ VideoGenTransformer/xtend-gen/VideoGenTest1XtendVersion.java
@@ -6,8 +6,12 @@ import fr.istic.videoGen.OptionalMedia;
 import fr.istic.videoGen.VideoDescription;
 import fr.istic.videoGen.VideoGeneratorModel;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Stack;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.xtext.xbase.lib.Conversions;
 import org.eclipse.xtext.xbase.lib.InputOutput;
 import org.junit.Assert;
 import org.junit.Test;
@@ -16,6 +20,9 @@ import org.junit.Test;
 public class VideoGenTest1XtendVersion {
   @Test
   public void testLoadModel() {
+    final ArrayList<MandatoryMedia> listMan = new ArrayList<MandatoryMedia>();
+    final ArrayList<OptionalMedia> listOp = new ArrayList<OptionalMedia>();
+    final ArrayList<AlternativesMedia> listAlt = new ArrayList<AlternativesMedia>();
     final VideoGeneratorModel videoGen = new VideoGenHelper().loadVideoGenerator(URI.createURI("specification.videogen"));
     Assert.assertNotNull(videoGen);
     EList<Media> _medias = videoGen.getMedias();
@@ -33,6 +40,7 @@ public class VideoGenTest1XtendVersion {
           long _length = f.length();
           String _plus_1 = (_plus + Long.valueOf(_length));
           InputOutput.<String>println(_plus_1);
+          listMan.add(man);
         }
       } else {
         if ((m instanceof OptionalMedia)) {
@@ -48,6 +56,7 @@ public class VideoGenTest1XtendVersion {
             long _length_1 = f_1.length();
             String _plus_3 = (_plus_2 + Long.valueOf(_length_1));
             InputOutput.<String>println(_plus_3);
+            listOp.add(op);
           }
         } else {
           if ((m instanceof AlternativesMedia)) {
@@ -63,11 +72,143 @@ public class VideoGenTest1XtendVersion {
                 long _length_2 = f_2.length();
                 String _plus_5 = (_plus_4 + Long.valueOf(_length_2));
                 InputOutput.<String>println(_plus_5);
+                listAlt.add(alt);
               }
             }
           }
         }
       }
+    }
+    final ArrayList result = this.calculateVariants(listMan, listOp, listAlt);
+  }
+  
+  public ArrayList calculateVariants(final ArrayList<MandatoryMedia> listMan, final ArrayList<OptionalMedia> listOp, final ArrayList<AlternativesMedia> listAlt) {
+    int _size = listOp.size();
+    String _plus = ("listOp.size() = " + Integer.valueOf(_size));
+    String _plus_1 = (_plus + " listAlt.size() = ");
+    int _size_1 = listAlt.size();
+    String _plus_2 = (_plus_1 + Integer.valueOf(_size_1));
+    InputOutput.<String>println(_plus_2);
+    int _size_2 = listOp.size();
+    int _multiply = (_size_2 * 2);
+    int _size_3 = listAlt.size();
+    final int resultNumber = (_multiply * _size_3);
+    ArrayList<ArrayList<Media>> result = this.initResult(listMan, resultNumber);
+    ArrayList<ArrayList<OptionalMedia>> optionelCombinations = new ArrayList<ArrayList<OptionalMedia>>();
+    this.recursive_combinations_start(listOp, optionelCombinations);
+    int j = 0;
+    int i = 0;
+    int _size_4 = result.size();
+    String _plus_3 = ("size = " + Integer.valueOf(_size_4));
+    InputOutput.<String>println(_plus_3);
+    while ((j < resultNumber)) {
+      {
+        for (final AlternativesMedia alt : listAlt) {
+          {
+            result.get(j).add(alt);
+            int _size_5 = optionelCombinations.size();
+            boolean _lessThan = (i < _size_5);
+            if (_lessThan) {
+              InputOutput.<Integer>println(Integer.valueOf(optionelCombinations.get(i).size()));
+              result.get(j).addAll(optionelCombinations.get(i));
+            }
+            j++;
+          }
+        }
+        i++;
+      }
+    }
+    return result;
+  }
+  
+  public ArrayList<ArrayList<Media>> insertOp(final ArrayList<OptionalMedia> listOp, final int size, final ArrayList<ArrayList<Media>> resultWithMan) {
+    ArrayList<ArrayList<Media>> result = new ArrayList<ArrayList<Media>>(resultWithMan);
+    Stack<OptionalMedia> listOpTemp = new Stack<OptionalMedia>();
+    listOpTemp.addAll(listOp);
+    int i = 0;
+    int j = 0;
+    int c = 0;
+    OptionalMedia head = null;
+    ArrayList<ArrayList<OptionalMedia>> used = new ArrayList<ArrayList<OptionalMedia>>();
+    Stack<OptionalMedia> tail = new Stack<OptionalMedia>();
+    ArrayList<OptionalMedia> combo = new ArrayList<OptionalMedia>();
+    while ((j < resultWithMan.size())) {
+      {
+        boolean _isEmpty = listOpTemp.isEmpty();
+        boolean _not = (!_isEmpty);
+        if (_not) {
+          head = listOpTemp.pop();
+          tail.addAll(listOpTemp);
+        }
+        combo.add(head);
+        if ((i == 0)) {
+          combo.addAll(tail);
+        }
+        boolean _contain = this.contain(used, combo);
+        boolean _not_1 = (!_contain);
+        if (_not_1) {
+          while ((c < size)) {
+            {
+              result.get(j).addAll(combo);
+              c++;
+              j++;
+            }
+          }
+        }
+      }
+    }
+    return result;
+  }
+  
+  public boolean contain(final ArrayList<ArrayList<OptionalMedia>> used, final ArrayList<OptionalMedia> combo) {
+    boolean compar = false;
+    for (final ArrayList<OptionalMedia> l : used) {
+      {
+        InputOutput.<String>println(((("l: " + l) + " combo: ") + combo));
+        HashSet<OptionalMedia> _hashSet = new HashSet<OptionalMedia>(l);
+        HashSet<OptionalMedia> _hashSet_1 = new HashSet<OptionalMedia>(combo);
+        compar = _hashSet.equals(_hashSet_1);
+        if (compar) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  
+  public ArrayList<ArrayList<Media>> initResult(final ArrayList<MandatoryMedia> listMan, final int size) {
+    final ArrayList<ArrayList<Media>> result = new ArrayList<ArrayList<Media>>();
+    int i = 0;
+    while ((i < size)) {
+      {
+        ArrayList<Media> _arrayList = new ArrayList<Media>(listMan);
+        result.add(_arrayList);
+        i++;
+      }
+    }
+    return result;
+  }
+  
+  public void recursive_combinations(final ArrayList<OptionalMedia> combination, final int ndx, final ArrayList<OptionalMedia> elems, final ArrayList<ArrayList<OptionalMedia>> result) {
+    int _length = ((Object[])Conversions.unwrapArray(elems, Object.class)).length;
+    boolean _equals = (ndx == _length);
+    if (_equals) {
+      result.add(combination);
+    } else {
+      combination.add(elems.get(ndx));
+      this.recursive_combinations(combination, (ndx + 1), elems, result);
+      combination.remove(elems.get(ndx));
+      this.recursive_combinations(combination, (ndx + 1), elems, result);
+    }
+  }
+  
+  public void recursive_combinations_start(final ArrayList<OptionalMedia> elems, final ArrayList<ArrayList<OptionalMedia>> result) {
+    final ArrayList<OptionalMedia> combination = new ArrayList<OptionalMedia>();
+    this.recursive_combinations(combination, 0, elems, result);
+    for (final ArrayList<OptionalMedia> l : result) {
+      int _size = l.size();
+      String _plus = ("combSize: " + Integer.valueOf(_size));
+      InputOutput.<String>println(_plus);
     }
   }
 }
